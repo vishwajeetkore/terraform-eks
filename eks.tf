3da653136c5332cb34b2be6126a2e6b442b8e358
Vishwajeet Kore <vishwkore@gmail.com>
2025-10-17 10:16:45 +0530

    terrafrom eks till 6.44 yt

--- eks.tf
+++ eks.tf
@@ -1,1 +1,17 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.0.0"
 
+  cluster_name = local.name
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+subnet_ids = module.vpc.private_subnets
+tags = {
+
+    Environment = local.env
+    terraform = "true"
+}
+
+
+}
---
+++ local.tf
@@ -0,0 +1,10 @@
+locals {
+   region = "ap-south-1"
+   name = "guru-eks-cluster"
+   vpc_cidr = "10.0.0.0/16"
+   azs = ["ap-south-1a", "ap-south-1b"]
+   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+   public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+   env = "dev"
+
+}
